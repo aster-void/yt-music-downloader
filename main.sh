4621ec06b5da953fb28ce0dbc5b1984f2c2cbe3b
aster <137767097+aster-void@users.noreply.github.com>
2024-08-12 23:04:11 +0900

    fix audio files being shuffled

--- main.sh
+++ main.sh
@@ -9,11 +9,12 @@ for link in "${links[@]}"; do
   yt-dlp --paths "./tmp/rename" -- "$link" >/dev/null 2>&1
   mv ./tmp/rename/* "./tmp/$idx.mp4"
   ffmpeg -i "./tmp/$idx.mp4" -acodec libmp3lame -aq 4 "./tmp/out/$idx.mp3" >/dev/null 2>&1
+  echo "./out/$idx.mp4" >> ./tmp/concat.txt
+
   idx=$(calc "$idx+1" | tr -d " \t")
-  echo $idx out of $total
+  echo $idx out of $total done
 done
 
-ls ./tmp/out | sed "s/^/file '.\/out\//" | sed "s/$/'/" > ./tmp/concat.txt
 ffmpeg -f concat -safe 0 -i ./tmp/concat.txt -c copy ./tmp/out/concat.mp3
 if [ -d ./out ]; then mv out out.bak; fi
 mv tmp/out out
