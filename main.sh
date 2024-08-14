5de3efcc3bdc2ff7886fefb5dad9ca7c32fa9cbf
aster <137767097+aster-void@users.noreply.github.com>
2024-08-15 02:03:03 +0900

    fix

--- main.sh
+++ main.sh
@@ -2,14 +2,15 @@
 rm -r ./tmp
 mkdir -p tmp/out
 mkdir tmp/rename
+mkdir tmp/in
 links=(`cat link.txt`)
 total=$(cat link.txt | wc -l)
 idx=0
 for link in "${links[@]}"; do
   yt-dlp --paths "./tmp/rename" -- "$link" >/dev/null 2>&1
-  mv ./tmp/rename/* "./tmp/$idx.mp4"
-  ffmpeg -i "./tmp/$idx.mp4" -acodec libmp3lame -aq 4 "./tmp/out/$idx.mp3" >/dev/null 2>&1
-  echo "./out/$idx.mp4" >> ./tmp/concat.txt
+  mv ./tmp/rename/* "./tmp/in/$idx.mp4"
+  ffmpeg -i "./tmp/in/$idx.mp4" -acodec libmp3lame -aq 4 "./tmp/out/$idx.mp3" # >/dev/null 2>&1
+  echo "file './out/$idx.mp3'" >> ./tmp/concat.txt
 
   idx=$(calc "$idx+1" | tr -d " \t")
   echo $idx out of $total done
@@ -18,5 +19,5 @@ done
 ffmpeg -f concat -safe 0 -i ./tmp/concat.txt -c copy ./tmp/out/concat.mp3
 if [ -d ./out ]; then mv out out.bak; fi
 mv tmp/out out
-rm -rf ./tmp
+# rm -rf ./tmp
 echo "Operation complete."
